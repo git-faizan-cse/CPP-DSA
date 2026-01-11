07daaeded79103d4645347ecc16635d6ac4d1a0c
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-11 18:04:20 +0530

    feat: added search and insert element functions in linked list.

--- lecture55(linkedList)/code.cpp
+++ lecture55(linkedList)/code.cpp
@@ -66,6 +66,45 @@ class List{
             head = newNode;
         }
     }
+    
+    void insert(int val, int pos){
+        if(pos < 0){
+            cout << "Invalid Position" << endl;
+            return;
+        }
+        if(pos == 0){
+            push_front(val);
+            return;
+        }
+        
+        Node* temp = head;
+        for(int i = 0; i < pos - 1; i++){
+            if(temp == NULL){
+                cout << "Invalid Position" << endl;
+                return;
+            }
+            temp = temp->next;
+        }
+        
+        Node* newNode = new Node(val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        
+        
+    }
+    int search(int val){
+        Node* temp = head;
+        int idx = 0;
+        cout << endl;
+        while ( temp != NULL){
+            if(temp-> data == val){
+                return idx;
+            }
+            temp = temp->next;
+            idx++;
+        }
+        return -1;
+    }
     void printLL() {
         Node* temp = head;
         
@@ -85,12 +124,16 @@ int main(){
     ll.push_front(3);
     
     ll.push_back(4);
+    ll.push_back(5);
+    
+    ll.insert(10, 2);
     
     ll.pop_front();
     ll.pop_back();
     
     ll.printLL();
     
+    cout << ll.search(1);
     
     
     return 0;
