6d25f6732976bf8497aba91bf1504d4baf88a5cd
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-19 17:10:22 +0530

    feat(lecture59(Queue)): implemented circular queue using array.

---
+++ lecture59(Queue)/circularQueue.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+class CircularQueue{
+  int* arr;
+  int currSize, cap;
+  int f, r;
+  public:
+  
+  CircularQueue(int size){
+      cap = size;
+      arr = new int[cap];
+      currSize = 0;
+      f = 0;
+      r = -1;
+  }
+  void push(int data){
+      if(currSize == cap){
+          cout << "Circular Queue is full" << endl;
+          return;
+      }
+      r = (r + 1) % cap;
+      arr[r] = data;
+      currSize++;
+  }
+  
+  void pop(){
+    if(empty()){
+        cout << "Circular Queue is Empty" << endl; 
+        return;
+    }
+    f = (f + 1) % cap;
+    currSize--;
+  }
+  
+  int front(){
+    if(empty()){
+        cout << "Circular Queue is Empty" << endl; 
+        return -1;
+    }
+    return arr[f];
+  }
+  
+  bool empty(){
+      return currSize == 0;
+  }
+  
+  void printArr(){
+      for(int i = 0; i < cap; i++){
+          cout << arr[i] << " ";
+      }
+      cout << endl;
+  }
+};
+int main(){
+    CircularQueue cq(3);
+    
+    cq.push(1);
+    cq.push(2);
+    cq.push(3);
+    cq.pop();
+    cq.push(4);
+    
+    cq.printArr();
+    
+    // now printing circular queue
+    
+    while(!cq.empty()){
+        cout << cq.front() << " ";
+        cq.pop();
+    }
+    
+    return 0;
+}
