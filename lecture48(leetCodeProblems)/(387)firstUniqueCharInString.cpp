ffd34683d5313279020fb49f9e8d7b48ef4bc133
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-19 20:50:40 +0530

    feat(lecture48(leetCodeProblems)): implemented first unique character in string problem no 387 on leetcode.

---
+++ lecture48(leetCodeProblems)/(387)firstUniqueCharInString.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+        // map 2 pass method
+        unordered_map<char,int> m;
+
+        for(char ch : s){
+            m[ch]++;
+        }
+        for(int i = 0 ; i < s.size(); i++){
+            if ( m[s[i]] == 1){
+                return i;
+            }
+        }
+        return -1;
+
+        // map + queue method.
+
+        // unordered_map<char,int> m;
+        // queue<int> Q;
+
+        // for(int i = 0; i < s.size(); i++){
+        //     if(m.find(s[i]) == m.end()){
+        //         Q.push(i);
+        //     }
+        //     m[s[i]]++;
+        //     while(Q.size() > 0 && m[s[Q.front()]] > 1){
+        //         Q.pop();
+        //     }
+        // }
+        // return Q.empty() ? -1 : Q.front();
+    }
+};
