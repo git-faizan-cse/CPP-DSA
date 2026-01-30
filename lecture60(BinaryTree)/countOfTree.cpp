c9a80cb07bf085c8e3757b4e27eddb104b2f018a
git-faizan-cse <mail.faizan.cse@gmail.com>
2026-01-31 04:04:11 +0530

    feat(lecture60(BinaryTree)): implemented Count of Tree in Binary Tree in Cpp.

---
+++ lecture60(BinaryTree)/countOfTree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* left;
+  Node* right;
+  
+  Node(int val){
+      data = val;
+      left = right = NULL;
+  }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preOrder){
+    idx++;
+    if(preOrder[idx] == -1) return NULL;
+    
+    Node *root = new Node(preOrder[idx]);
+    root->left = buildTree(preOrder);
+    root->right = buildTree(preOrder);
+    
+    return root;
+}
+
+int countOfTree(Node *root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftCount = countOfTree(root->left);
+    int rightCount = countOfTree(root->right);
+    
+    return (leftCount+rightCount)+1;
+}
+
+int main(){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node* root = buildTree(preorder);
+
+    int count = countOfTree(root);
+    cout << "Count of the tree: " << count << endl;
+
+    return 0;
+}
